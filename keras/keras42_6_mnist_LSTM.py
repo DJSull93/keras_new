7f1c03e8f13de94e295f0fc47c96584e03f84104
DJSull93 <digi809@naver.com>
2021-07-20 17:45:28 +0900

    middlee

---
+++ keras/keras42_6_mnist_LSTM.py
@@ -0,0 +1,80 @@
+# mnist example 
+# make model to LSTM
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
+
+# 1. data
+# y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+(x_train, y_train), (x_test, y_test) = mnist.load_data() # (60000, 28, 28) (60000,) (10000, 28, 28) (10000,)
+
+x_train = x_train.reshape(60000, 28*28*1)
+x_test = x_test.reshape(10000, 28*28*1)
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler, QuantileTransformer, PowerTransformer
+scaler = QuantileTransformer()
+# scaler.fit(x_train) 
+# x_train = scaler.transform(x_train) 
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test) 
+
+x_train = x_train.reshape(60000, 28*28, 1)
+x_test = x_test.reshape(10000, 28*28, 1)
+
+from sklearn.preprocessing import OneHotEncoder
+one = OneHotEncoder()
+y_train = y_train.reshape(-1,1)
+y_test = y_test.reshape(-1,1)
+one.fit(y_train)
+y_train = one.transform(y_train).toarray() # (60000, 10)
+y_test = one.transform(y_test).toarray() # (10000, 10)
+
+# 2. model
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, LSTM, Dropout, Input
+
+input1 = Input(shape=(28*28, 1))
+xx = LSTM(units=20, activation='relu')(input1)
+# xx = Dense(128, activation='relu')(xx)
+# xx = Dense(64, activation='relu')(xx)
+xx = Dense(32, activation='relu')(xx)
+xx = Dense(16, activation='relu')(xx)
+output1 = Dense(10, activation='softmax')(xx)
+
+model = Model(inputs=input1, outputs=output1)
+
+# 3. comple fit // metrics 'acc'
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='acc')
+
+from tensorflow.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='val_loss', patience=20, mode='min', verbose=1)
+
+import time
+
+start_time = time.time()
+model.fit(x_train, y_train, epochs=1000, batch_size=256, verbose=2,
+    validation_split=0.0025, callbacks=[es])
+end_time = time.time() - start_time
+
+# 4. predict eval -> no need to
+
+loss = model.evaluate(x_test, y_test)
+print("time : ", end_time)
+print('loss[category] : ', loss[0])
+print('loss[accuracy] : ', loss[1])
+
+'''
+CNN
+time =  12.263086080551147
+loss :  0.08342713862657547
+acc :  0.9821000099182129
+
+DNN
+time :  20.324632167816162
+loss[category] :  0.09825558215379715
+loss[accuracy] :  0.9785000085830688
+
+LSTM
+
+'''
