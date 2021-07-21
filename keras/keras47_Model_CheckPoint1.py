f4957252aa07c3221e5b6777fb316bb85803294e
DJSull93 <digi809@naver.com>
2021-07-21 17:02:33 +0900

    lllll

--- keras/keras47_Model_CheckPoint1.py
+++ keras/keras47_Model_CheckPoint1.py
@@ -30,28 +30,22 @@ output1 = Dense(1)(dense4)
 
 model = Model(inputs=input1, outputs=output1)
 
-# model.save('./_save/keras46_1_save_model_3.h5')
-# model.save_weights('./_save/keras46_1_save_weight_1.h5')
-
-# model.load_weights('./_save/keras46_1_save_weight_1.h5')
-
-
 # 3. 컴파일 훈련
 model.compile(loss='mse', optimizer='adam')
 
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 es = EarlyStopping(monitor='val_loss', patience=30, mode='min', verbose=1)
+cp = ModelCheckpoint(monitor='val_loss',save_best_only=True, mode='auto',
+                     filepath='./_save/ModelCheckPoint/keras47_MCP.hdf5')
 
 import time 
 start_time = time.time()
-model.load_weights('./_save/keras46_1_save_weight_2.h5')
-# model.fit(x_train, y_train, epochs=1000, batch_size=32,
-#  verbose=2, validation_split=0.22, shuffle=True, 
-#  callbacks=[es])
+model.fit(x_train, y_train, epochs=1000, batch_size=32,
+ verbose=2, validation_split=0.22, shuffle=True, 
+ callbacks=[es, cp])
 end_time = time.time() - start_time
 
-# model.save('./_save/keras46_1_save_model_4.h5')
-# model.save_weights('./_save/keras46_1_save_weight_2.h5')
+model.save('./_save/ModelCheckPoint/keras47_model_save.h5')
 
 # 4. 평가 예측
 y_predict = model.predict([x_test])
@@ -61,3 +55,10 @@ print('loss : ', loss)
 
 r2 = r2_score(y_test, y_predict)
 print('R^2 score : ', r2)
+
+'''
+Epoch 00172: early stopping
+total time :  8.910428524017334
+loss :  2321.75048828125
+R^2 score :  0.5975857448310755
+'''
