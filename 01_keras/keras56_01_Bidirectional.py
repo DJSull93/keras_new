41927c6fa386848db3636ac06e0f3645b4ab1697
DJSull93 <digi809@naver.com>
2021-07-27 23:21:18 +0900

    dacon

---
+++ 01_keras/keras56_01_Bidirectional.py
@@ -0,0 +1,73 @@
+# LSTM 시계열 끝판왕 
+
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, SimpleRNN, LSTM, Bidirectional
+
+# 1. data
+
+x = np.array([[1,2,3], [2,3,4], [3,4,5], [4,5,6]]) # (4, 3) 
+y = np.array([4,5,6,7]) # (4, )
+
+# print(x.shape, y.shape)
+
+x = x.reshape(4, 3, 1) # (batch_size, timesteps, feature)
+
+# 2. model
+
+model = Sequential()
+# model.add(SimpleRNN(units=10, activation='relu', input_shape=(3, 1)))
+model.add(LSTM(units=10, activation='relu', input_shape=(3, 1), return_sequences=True))
+model.add(LSTM(units=10, activation='relu'))
+model.add(Dense(5, activation='relu'))
+model.add(Dense(1))
+'''
+# 3. compile train
+
+model.compile(loss='mse', optimizer='adam')
+
+from tensorflow.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='loss', patience=20, mode='min', verbose=1)
+
+model.fit(x, y, epochs=1000, batch_size=1, callbacks=[es])
+
+# 4. pred eval
+x_input = np.array([5, 6, 7]).reshape(1, 3, 1)
+y_pred = model.predict(x_input)
+print(y_pred) # [[8.006084]] epochs = 1745
+'''
+model.summary()
+
+# bidirec
+# _________________________________________________________________
+# Layer (type)                 Output Shape              Param #
+# =================================================================
+# lstm (LSTM)                  (None, 3, 10)             480
+# _________________________________________________________________
+# bidirectional (Bidirectional (None, 20)                1680
+# _________________________________________________________________
+# dense (Dense)                (None, 5)                 105
+# _________________________________________________________________
+# dense_1 (Dense)              (None, 1)                 6
+# =================================================================
+# Total params: 2,271
+# Trainable params: 2,271
+# Non-trainable params: 0
+# _________________________________________________________________
+
+# non bidirec
+# _________________________________________________________________
+# Layer (type)                 Output Shape              Param #
+# =================================================================
+# lstm (LSTM)                  (None, 3, 10)             480
+# _________________________________________________________________
+# lstm_1 (LSTM)                (None, 10)                840
+# _________________________________________________________________
+# dense (Dense)                (None, 5)                 55
+# _________________________________________________________________
+# dense_1 (Dense)              (None, 1)                 6
+# =================================================================
+# Total params: 1,381
+# Trainable params: 1,381
+# Non-trainable params: 0
+# _________________________________________________________________
---
+++ 01_keras/keras58_Tensorboard2.py
@@ -0,0 +1,51 @@
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 1. 데이터
+x = np.array([1,2,3,4,5])
+y = np.array([1,2,4,3,5])
+
+# 2. 모델 구성
+model = Sequential()
+model.add(Dense(5, input_dim=1))
+model.add(Dense(3))
+model.add(Dense(3))
+model.add(Dense(3))
+model.add(Dense(3))
+model.add(Dense(3))
+model.add(Dense(4))
+model.add(Dense(1))
+
+# 3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+
+tb = TensorBoard(log_dir='./_save/_graph', histogram_freq=0,
+                write_graph=True, write_images=True)
+
+model.fit(x, y, epochs=1000, batch_size=1, callbacks=[tb],
+            validation_split=0.2)
+
+# 4. 평가 예측
+loss = model.evaluate(x, y)
+print('loss : ', loss)
+result = model.predict([6])
+print('6의 예측값 : ', result)
+
+y_predict = model.predict(x)
+# plt.scatter(x,y)
+# plt.plot(x,y_predict, color='red')
+# plt.show()
+
+'''
+cmd
+d:
+cd study
+cd _save
+cd _graph
+dir/w
+tensorboard --logdir=.
+http://localhost:6006/
+'''
