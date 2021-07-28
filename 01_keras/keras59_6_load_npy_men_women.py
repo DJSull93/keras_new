96bcb7900efa9926606836b73fd246a365015bc7
DJSull93 <digi809@naver.com>
2021-07-28 17:16:09 +0900

    wtd

--- 01_keras/keras59_6_load_npy_men_women.py
+++ 01_keras/keras59_6_load_npy_men_women.py
@@ -16,19 +16,19 @@ x_pred = np.load('./_save/_NPY/k59_mw_x_pred.npy')
 
 # 2. model
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, GlobalAveragePooling2D
 
 model = Sequential()
 
 model.add(Conv2D(filters = 32, kernel_size=(3,3), input_shape =(150,150,3), activation= 'relu'))
 model.add(Conv2D(filters = 32, kernel_size=(3,3),  activation= 'relu'))
-# model.add(MaxPooling2D(2,2))
-model.add(Conv2D(filters = 64, kernel_size=(2,2),  activation= 'relu'))
-model.add(Conv2D(filters = 64, kernel_size=(3,3),  activation= 'relu'))
-# model.add(MaxPooling2D(2,2))
+model.add(MaxPooling2D(2,2))
+model.add(Conv2D(filters = 128, kernel_size=(3,3),  activation= 'relu'))
+model.add(Conv2D(filters = 128, kernel_size=(3,3),  activation= 'relu'))
+model.add(MaxPooling2D(2,2))
 model.add(Flatten())
 model.add(Dense(128, activation= 'relu'))
-model.add(Dense(64, activation= 'relu'))
+model.add(Dense(32, activation= 'relu'))
 model.add(Dense(1, activation= 'sigmoid'))
 
 # 3. compile train
---
+++ 01_keras/keras59_7_save_npy_rps.py
@@ -0,0 +1,47 @@
+
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    horizontal_flip=True,
+    vertical_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    rotation_range=5,
+    zoom_range=1.2,
+    shear_range=0.7,
+    fill_mode='nearest',
+    validation_split=0.25
+)
+
+# 통상적으로 테스트셋은 증폭하지 않음
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+xy_train = train_datagen.flow_from_directory(
+    '../_data/rps',
+    target_size=(150, 150),
+    batch_size=2000,
+    class_mode='categorical',
+    shuffle=True,
+    subset='training'
+)
+
+xy_test = train_datagen.flow_from_directory(
+    '../_data/rps',
+    target_size=(150, 150),
+    batch_size=2000,
+    class_mode='categorical',
+    shuffle=True,
+    subset='validation'
+)
+
+# np.save('./_save/_NPY/k59_rps_x_train', arr=xy_train[0][0])
+# np.save('./_save/_NPY/k59_rps_y_train', arr=xy_train[0][1])
+# np.save('./_save/_NPY/k59_rps_x_test', arr=xy_test[0][0])
+# np.save('./_save/_NPY/k59_rps_y_test', arr=xy_test[0][1])
+
+print(xy_train[0][0]) # 
+print(xy_train[0][1]) # 
+print(xy_train[0][0].shape) # (1890, 150, 150, 3)
+print(xy_train[0][1].shape) # (1890, 3)
