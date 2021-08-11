db339792e744f51e866f33dbf6d24975c180b81f
DJSull93 <digi809@naver.com>
2021-08-11 16:46:09 +0900

    edit

--- 03_ML/M10_01_pipeline_randSearch_05_wine.py
+++ 03_ML/M10_01_pipeline_randSearch_05_wine.py
@@ -21,16 +21,17 @@ from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, KFold, cro
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler, QuantileTransformer, PowerTransformer
 
 parameters = [{
-    "randomforestclassifier__max_depth": [6, 8, 10, 12],
-    "randomforestclassifier__min_samples_leaf": [3, 5, 7],
-    "randomforestclassifier__min_samples_split": [2, 3, 5, 10],
+    "rf__max_depth": [6, 8, 10, 12],
+    "rf__min_samples_leaf": [3, 5, 7],
+    "rf__min_samples_split": [2, 3, 5, 10],
 }] # model and param connection : model__param
 
 n_split = 5
 kfold = KFold(n_splits=n_split, shuffle=True, 
                 random_state=66)
 
-pipe = make_pipeline(MinMaxScaler(), RandomForestClassifier())
+# pipe = make_pipeline(MinMaxScaler(), RandomForestClassifier())
+pipe = Pipeline([("scaler", MinMaxScaler()), ("rf", RandomForestClassifier())])
 
 # model = RandomizedSearchCV(pipe, parameters, cv=kfold, verbose=1)
 model = GridSearchCV(pipe, parameters, cv=kfold, verbose=1)
@@ -45,6 +46,7 @@ et = time.time() - st
 
 print('totla time : ', et)
 print('Best estimator : ', model.best_estimator_)
+print('Best params : ', model.best_params_)
 print('Best score  :', model.best_score_)
 
 '''
@@ -63,5 +65,13 @@ Best estimator :  Pipeline(steps=[('minmaxscaler', MinMaxScaler()),
                  RandomForestClassifier(max_depth=12, min_samples_leaf=3,
                                         min_samples_split=3))])
 Best score  : 0.6484271747514123
+
+totla time :  83.50680041313171
+Best estimator :  Pipeline(steps=[('scaler', MinMaxScaler()),
+                ('rf',
+                 RandomForestClassifier(max_depth=12, min_samples_leaf=3,
+                                        min_samples_split=5))])
+Best params :  {'rf__max_depth': 12, 'rf__min_samples_leaf': 3, 'rf__min_samples_split': 5}
+Best score  : 0.6472045610889913
 '''
 
